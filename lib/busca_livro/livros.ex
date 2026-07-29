675e4fa16f5104b356980b967ccbaa96702cdfdd
otaviouu <otavio.venancio.oliveira@usp.br>
2026-07-28 21:29:42 -0300

    show livrios resources in admin page

--- lib/busca_livro/livros.ex
+++ lib/busca_livro/livros.ex
@@ -1,6 +1,11 @@
 defmodule BuscaLivro.Livros do
   use Ash.Domain,
-    otp_app: :busca_livro
+    otp_app: :busca_livro,
+    extensions: [AshAdmin.Domain]
+
+  admin do
+    show? true
+  end
 
   resources do
     resource BuscaLivro.Livros.Livro
