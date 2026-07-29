e4d601675b993e96bd82d2a630d6ac65abfc6006
otaviouu <otavio.venancio.oliveira@usp.br>
2026-07-28 23:53:21 -0300

    improve data table

--- lib/busca_livro_web/live/livros_live/index.ex
+++ lib/busca_livro_web/live/livros_live/index.ex
@@ -4,8 +4,36 @@ defmodule BuscaLivroWeb.LivrosLive.Index do
   def render(assigns) do
     ~H"""
     <Layouts.app {assigns}>
-      <Cinder.collection resource={BuscaLivro.Livros.Livro}>
-        <:col :let={livro} field="titulo" search sort>{livro.titulo}</:col>
+      <Cinder.collection
+        query={
+          Ash.Query.for_read(BuscaLivro.Livros.Livro, :read)
+          |> Ash.Query.load([:loja, :full_image_url])
+        }
+        layout={:grid}
+        grid_columns={2}
+      >
+        <:col field="titulo" search />
+        <:filter field="loja.nome" />
+
+        <:item :let={livro}>
+          <figure class="p-4">
+            <img
+              src={livro.full_image_url}
+              alt={livro.titulo}
+              class="h-48 w-auto object-contain"
+            />
+          </figure>
+
+          <div class="card-body p-4">
+            <h2 class="card-title text-base line-clamp-2">
+              {livro.titulo}
+            </h2>
+
+            <div class="badge badge-outline">
+              {livro.loja.nome}
+            </div>
+          </div>
+        </:item>
       </Cinder.collection>
     </Layouts.app>
     """
